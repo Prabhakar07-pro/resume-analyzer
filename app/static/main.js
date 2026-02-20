f792a7218e1db35ab55e715d6de0fcf690d857cf
Prabhakar <prabhakarramaphalam07@gmail.com>
2026-02-20 12:40:12 +0530

    layout changes

--- app/static/main.js
+++ app/static/main.js
@@ -4,3 +4,32 @@ function goToTop() {
         behavior: "smooth"
     });
 }
+
+
+document.addEventListener("DOMContentLoaded", function () {
+    document.querySelector(".top").style.display = "none";
+});
+
+window.addEventListener("scroll", function () {
+
+    const button = document.querySelector(".top");
+
+    const scrollPosition = window.scrollY;
+    const pageHeight = document.documentElement.scrollHeight;
+    const windowHeight = window.innerHeight;
+
+    const halfwayPoint = (pageHeight - windowHeight) / 2;
+
+    if (scrollPosition > halfwayPoint) {
+        button.style.display = "block";
+    } else {
+        button.style.display = "none";
+    }
+});
+
+function goToTop() {
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    });
+}
